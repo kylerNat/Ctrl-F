3271e5be687eef9ce4a2d2cb95b5a65db5e06cb5
kylerNat <kylerNat@github.com>
2017-04-01 02:51:08 -0700

    hi

---
+++ Ctrl-F/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ctrl-F
+//
+//  Created by Kyler Natividad on 4/1/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ctrl-F/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ctrl-F
+//
+//  Created by Kyler Natividad on 4/1/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
